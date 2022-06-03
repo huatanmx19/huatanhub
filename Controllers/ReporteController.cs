c3e29772f9d3b3353c8b83d4b60623a415f59b52
Hiram García <98775186+Und3rgr0und1@users.noreply.github.com>
2022-06-03 17:43:51 -0500

    Reporte Corregido

--- Controllers/ReporteController.cs
+++ Controllers/ReporteController.cs
@@ -32,17 +32,6 @@ namespace HuatanHub.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<ReporteResponse>> GetReporteTramo(int id)
         {
-            //Ajuste
-            //SqlConnection con = new SqlConnection();
-            //con.ConnectionString =
-            //  "Data Source=34.71.46.142;" +
-            //  "Initial Catalog=huatan;" +
-            //  "User id=sqlserver;" +
-            //  "Password=qiGMdk8G3H7LqIG3;";
-            //Ajuste
-            //var reporte = new List<ReporteResponse>();
-            //var reporte = new ReporteResponse();
-
             //try
             //{
             var backTime = Environment.GetEnvironmentVariable("DELTA_MINUTES") ?? "-60";
@@ -75,7 +64,9 @@ namespace HuatanHub.Controllers
             if (id > 0)
                 queryAttendanceToday = queryAttendanceToday.Where(x => x.Empleado.TramoId == id);
 
-            var attendanceToday = await queryAttendanceToday.CountAsync();
+            var attendanceToday = await queryAttendanceToday
+                .GroupBy(x => x.EmpleadoId)
+                .CountAsync();
 
             var queryEmployeeTotal = _context.Empleados
                 .Where(x => x.Active);
@@ -94,112 +85,6 @@ namespace HuatanHub.Controllers
                 Activos = activos,
                 Inactivos = inactivos
             };
-
-            ////ORIGINAL - FIN
-
-            //Ajuste
-            //con.Open();
-            ////var command = new SqlCommand(qReporte.Reporte, con);
-            ////command.CommandType = CommandType.StoredProcedure;
-            ////command.Parameters.AddWithValue("@id", id);
-            ////command.Parameters.AddWithValue("@fecha", delta);
-            //using (var rd = command.ExecuteReader())
-            //{
-            //    while (rd.Read())
-            //        reporte.Add(new ReporteResponse
-            //        {
-            //            Total = rd.IsDBNull(rd.GetOrdinal("Total")) ? 0 : Convert.ToInt32(rd["Total"]),
-            //            Asistencia = rd.IsDBNull(rd.GetOrdinal("Asistencia")) ? 0 : Convert.ToInt32(rd["Asistencia"]),
-            //            Activos = rd.IsDBNull(rd.GetOrdinal("Activos")) ? 0 : Convert.ToInt32(rd["Activos"]),
-            //            Inactivos = rd.IsDBNull(rd.GetOrdinal("Inactivos")) ? 0 : Convert.ToInt32(rd["Inactivos"])
-            //        });
-            //}
-
-            //await con.OpenAsync().ConfigureAwait(continueOnCapturedContext: false);
-
-            //using (var reader = await command.ExecuteReaderAsync())
-            //{
-            //    if (await reader.ReadAsync())
-            //    {
-            //        reporte = new ReporteResponse();
-            //        reporte.Total = reader.IsDBNull(reader.GetOrdinal("Total")) ? 0 : Convert.ToInt32(reader["Total"]);
-            //        reporte.Asistencia = reader.IsDBNull(reader.GetOrdinal("Asistencia")) ? 0 : Convert.ToInt32(reader["Asistencia"]);
-            //        reporte.Activos = reader.IsDBNull(reader.GetOrdinal("Activos")) ? 0 : Convert.ToInt32(reader["Activos"]);
-            //        reporte.Inactivos = reader.IsDBNull(reader.GetOrdinal("Inactivos")) ? 0 : Convert.ToInt32(reader["Inactivos"]);
-            //    }
-            //}
-            ////return Ok(reporte);
-
-
-
-            ////////////////////////////////////////////////////////////////
-            //Tipo 1 -INICIO
-            ////////////////////////////////////////////////////////////////
-            //var reporte = new ReporteResponse();
-            //using (SqlConnection connection = new SqlConnection(con.ConnectionString))
-            //{
-            //    using (var command = new SqlCommand(qReporte.Reporte, connection))
-            //    {
-            //        command.CommandType = CommandType.StoredProcedure;
-
-            //        command.Parameters.AddWithValue("@id", id);
-            //        command.Parameters.AddWithValue("@fecha", delta);
-
-            //        await connection.OpenAsync();
-
-            //        using (var reader = await command.ExecuteReaderAsync())
-            //        {
-            //            if (await reader.ReadAsync())
-            //            {
-            //                reporte = new ReporteResponse();
-            //                reporte.Total = reader.IsDBNull(reader.GetOrdinal("Total")) ? 0 : Convert.ToInt32(reader["Total"]);
-            //                reporte.Asistencia = reader.IsDBNull(reader.GetOrdinal("Asistencia")) ? 0 : Convert.ToInt32(reader["Asistencia"]);
-            //                reporte.Activos = reader.IsDBNull(reader.GetOrdinal("Activos")) ? 0 : Convert.ToInt32(reader["Activos"]);
-            //                reporte.Inactivos = reader.IsDBNull(reader.GetOrdinal("Inactivos")) ? 0 : Convert.ToInt32(reader["Inactivos"]);
-            //            }
-            //        }
-            //    }
-            //}
-            //return Ok(reporte);
-            ////////////////////////////////////////////////////////////////
-            //Tipo 1 -FIN
-            ////////////////////////////////////////////////////////////////
-
-            ////////////////////////////////////////////////////////////////
-            //Tipo 2 -INICIO
-            ////////////////////////////////////////////////////////////////
-            //var reporte = new List<ReporteResponse>();
-            //using (SqlConnection connection = new SqlConnection(con.ConnectionString))
-            //{
-            //    using (var command = new SqlCommand(qReporte.Reporte, connection))
-            //    {
-            //        command.CommandType = CommandType.StoredProcedure;
-
-            //        command.Parameters.AddWithValue("@id", id);
-            //        command.Parameters.AddWithValue("@fecha", delta);
-
-            //        await connection.OpenAsync().ConfigureAwait(continueOnCapturedContext: false);
-
-            //        using (var reader = await command.ExecuteReaderAsync())
-            //        {
-            //            while (await reader.ReadAsync())
-            //            {
-            //                var item = new ReporteResponse
-            //                {
-            //                    Total = reader.IsDBNull(reader.GetOrdinal("Total")) ? 0 : Convert.ToInt32(reader["Total"]),
-            //                    Asistencia = reader.IsDBNull(reader.GetOrdinal("Asistencia")) ? 0 : Convert.ToInt32(reader["Asistencia"]),
-            //                    Activos = reader.IsDBNull(reader.GetOrdinal("Activos")) ? 0 : Convert.ToInt32(reader["Activos"]),
-            //                    Inactivos = reader.IsDBNull(reader.GetOrdinal("Inactivos")) ? 0 : Convert.ToInt32(reader["Inactivos"])
-            //                };
-            //                reporte.Add(item);
-            //            }
-            //        }
-            //    }
-            //}
-            //return Ok(reporte);
-            ////////////////////////////////////////////////////////////////
-            //Tipo 2 -FIN
-            ////////////////////////////////////////////////////////////////
 
             //Original
             return Ok(reporte);
